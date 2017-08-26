d4ef67bbec0bc965a2245b9cb39a7d9019b8489e
Dimitar Filipov <dfilipov94@gmail.com>
2017-08-26 16:10:16 +0300

    Added 2 conversion exercises

---
+++ Strings and Text Processing/Problem 1. Convert from Base-10 to Base-N/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Convert_from_Base_10_to_Base_N
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            List<BigInteger> commands = Console.ReadLine().Split(' ').Select(BigInteger.Parse).ToList();
+            BigInteger theBase = commands[0];
+            BigInteger theNumber = commands[1];
+            StringBuilder builder = new StringBuilder();
+
+            while (theNumber != 0)
+            {
+                builder.Insert(0,(theNumber % theBase));
+                theNumber /= theBase;
+            }
+
+            Console.WriteLine(builder);
+        }
+    }
+}
---
+++ Strings and Text Processing/Problem 2. Convert from Base-N to Base-10/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace Convert_from_Base_N_to_Base_10
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            List<string> commands = Console.ReadLine().Split(' ').ToList();
+            int theBase = int.Parse(commands[0]);
+            string theNumber = commands[1];
+            Console.WriteLine(ConvertFromBaseN(theNumber, theBase));
+        }
+
+        public static BigInteger ConvertFromBaseN(string number, int theBase)
+        {
+            return number.Select(digit => digit - 48).Aggregate((x, y) => x * theBase + y);
+        }
+    }
+}
