6d4f669b8adc9b8b267261d2172cf659289a8f8a
Dimitar Filipov <dfilipov94@gmail.com>
2017-08-26 15:22:56 +0300

    Added more exercises for objects and classes

---
+++ Objects, Classes, Files and Exceptions - More Exercises/OrderByAge/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderByAge
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            List<Student> students = new List<Student>();
+            while (input != "End")
+            {
+                List<string> commands = input.Split(' ').ToList();
+                students.Add(new Student(commands[0], commands[1], commands[2]));
+                input = Console.ReadLine();
+            }
+
+            var orderedStudents = students.OrderBy(x => x.age);
+            foreach (var student in orderedStudents)
+            {
+                student.Informator();
+            }
+
+        }
+    }
+
+    public class Student
+    {
+        public string name;
+        public string ID;
+        public int age;
+
+        public Student(string name, string ID, string age)
+        {
+            this.name = name;
+            this.ID = ID;
+            this.age = int.Parse(age);
+        }
+
+        public void Informator()
+        {
+            Console.WriteLine($"{name} with ID: {ID} is {age} years old.");
+        }
+    }
+}
---
+++ Objects, Classes, Files and Exceptions - More Exercises/VehicleCatalogue/VehicleCatalogue.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleCatalogue
+{
+    public class VehicleCatalogue
+    {
+        public static void Main()
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            string input = Console.ReadLine();
+            double truckHP = 0;
+            double carHP = 0;
+
+            while (input != "End")
+            {
+                List<string> commands = input.Split(' ').ToList();
+                vehicles.Add(new Vehicle(commands[0].ToLower(), commands[1], commands[2], commands[3]));
+                if (commands[0] == "truck")
+                {
+                    truckHP += double.Parse(commands[3]);
+                }
+                else
+                {
+                    carHP += double.Parse(commands[3]);
+                }
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+            input = char.ToUpper(input[0]) + input.Substring(1).ToLower();
+
+            while (input != "Close the catalogue")
+            {
+                vehicles.Find(x => x.model == input).Informator();
+                input = Console.ReadLine();
+                input = char.ToUpper(input[0]) + input.Substring(1).ToLower();
+            }
+
+            var trucks = vehicles.Where(x => x.type == "Truck");
+            var trucksAvgHP = truckHP / trucks.Count();
+            var cars = vehicles.Where(x => x.type == "Car");
+            var carsAvgHP = carHP / cars.Count();
+
+            Console.WriteLine($"Cars have average horsepower of: {(!Double.IsNaN(carsAvgHP) ? carsAvgHP : 0):F2}.");
+            Console.WriteLine($"Trucks have average horsepower of: {(!Double.IsNaN(trucksAvgHP) ? trucksAvgHP : 0):F2}.");
+        }
+    }
+
+    public class Vehicle
+    {
+        public string type;
+        public string model;
+        public string color;
+        public int horsePower;
+
+        public Vehicle(string type, string model, string color, string horsePower)
+        {
+            this.type = char.ToUpper(type[0]) + type.Substring(1).ToLower();
+            this.model = char.ToUpper(model[0]) + model.Substring(1).ToLower();
+            this.color = color;
+            this.horsePower = int.Parse(horsePower);
+        }
+
+        public void Informator()
+        {
+            Console.WriteLine($"Type: {type}");
+            Console.WriteLine($"Model: {model}");
+            Console.WriteLine($"Color: {color}");
+            Console.WriteLine($"Horsepower: {horsePower}");
+        }
+    }
+
+}
