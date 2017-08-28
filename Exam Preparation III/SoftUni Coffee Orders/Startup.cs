1f5b421cec9489b99a073a573bb798541a9019a3
Dimitar Filipov <dfilipov94@gmail.com>
2017-08-28 19:45:52 +0300

    Added Exam Preparation III alongside SoftUni Coffee Orders

---
+++ Exam Preparation III/SoftUni Coffee Orders/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Globalization;
+
+namespace SoftUni_Coffee_Orders
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int numOrders = int.Parse(Console.ReadLine());
+            decimal totalPrice = 0;
+            for (int i = 0; i < numOrders; i++)
+            {
+                decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
+                string theDate = Console.ReadLine();
+                DateTime orderDate = DateTime.ParseExact(theDate,"d/M/yyyy",CultureInfo.InvariantCulture);
+                long capsuleCount = long.Parse(Console.ReadLine());
+                int daysInMonth = DateTime.DaysInMonth(orderDate.Year, orderDate.Month);
+                decimal price = (daysInMonth * capsuleCount) * pricePerCapsule;
+                totalPrice += price;
+                Console.WriteLine($"The price for the coffee is: ${price:F2}");
+            }
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+            Console.WriteLine(int.MaxValue);
+        }
+    }
+}
